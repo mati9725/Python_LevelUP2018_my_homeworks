b4a60efd81f13d7b24ad86ee68051fb5f11b7c46
Mateusz_W <mati9725@wp.pl>
2018-03-12 21:43:11 +0100

    Pierwszy raz ma dzialac

--- app.py
+++ app.py
@@ -2,36 +2,37 @@ from flask import Flask
 from flask import request
 import time
 
-
 app = Flask(__name__)
+counter = 0
+
 
 @app.route('/')
 def hello():
-    return 'Hello!!!'
+    return 'Hello, World!'
 
 
-@app.route('/request')
+@app.route('/request')                      # usuń tę funkcję
 def request_info():
-    return f'request method: {request.method} url: {request.url} headers: {request.headers}'			#usuń tę funkcję
+    return f'request method: {request.method} url: {request.url} headers: {request.headers}'
 
 
 @app.route('/now')
 def time_now():
-    return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) + "." + str(int((time.time() % 1 ) * 10**6))
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) + "." + str(int((time.time() % 1) * 10 ** 6))
 
 
 @app.route('/user-agent')
 def user_agent():
-	return f'{request.user_agent.string}'
-    #return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) + "." + str(int((time.time() % 1 ) * 10**6))
+    platform = Mobile 
+    if request.user_agent.platform == "windows" or
+    return f'{request.user_agent.string}'
 
 
 @app.route('/counter')
-def time_now():
-    return 3
+def count_it():
+    counter += 1
+    return counter
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    counter = 0
-
