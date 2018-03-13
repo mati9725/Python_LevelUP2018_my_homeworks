9fdd35014007b05f04f6de40d8be7805775473e3
Mateusz_W <mati9725@wp.pl>
2018-03-13 18:49:42 +0100

    proba

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ from flask import Flask
 from flask import request
 import time
 
+
+class count_it:
+    def __init__(self):
+        self.counter = 0
+
+    def count_up(self):
+        self.counter += 1
+        return self.counter
+
+
 app = Flask(__name__)
-counter = 0
+counter1 = count_it()
 
 
 @app.route('/')
@@ -11,7 +21,7 @@ def hello():
     return 'Hello, World!'
 
 
-@app.route('/request')                      # usuń tę funkcję
+@app.route('/request')  # usuń tę funkcję
 def request_info():
     return f'request method: {request.method} url: {request.url} headers: {request.headers}'
 
@@ -21,10 +31,26 @@ def time_now():
     return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) + "." + str(int((time.time() % 1) * 10 ** 6))
 
 
+@app.route('/user-agent')
+def user_agent():
+    name = request.user_agent.platform
+    name = name.lower()
+    if name == "windows" or name == "macos" or name == "linux":
+        response = f'PC / {name[0].upper()}'
+    else:
+        response = f'Mobile / {name[0].upper()}'
+    name = request.user_agent.platform
+    response += name[1:] + " / "
+    name = request.user_agent.browser
+    response += name[0].upper()
+    response += name[1:]
+    response = response + " " + request.user_agent.version
+    return response
+
+
 @app.route('/counter')
 def count_it():
-    counter += 1
-    return counter
+    return str(counter1.count_up())
 
 
 if __name__ == '__main__':
